8104331790aef0883cb0d72f45e58865e8bd7cc8
Everton Silva <evertom.silva@outlook.com.br>
2024-09-19 20:16:13 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,33 @@
+alert('Seja Bem Vindo ao Jogo do Número Secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let palpite;
+let tentativas = 1;
+
+//while é um comando de repetição
+while (palpite != numeroSecreto) {
+   palpite = prompt(`Escolha um numero entre 1 e ${numeroMaximo}.`);
+   if (palpite == numeroSecreto) {
+      break;
+   } else {
+      if (palpite > numeroSecreto) {
+         alert('Seu palpite é maior que o numero secreto! Tente novamente.');
+      } else {
+         if (palpite < numeroSecreto) {
+            alert('Seu palpite é menor que o numero secreto! Tente novamente.');
+         }
+         //tentativas = tentativas + 1;
+         tentativas++
+      }
+   }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//    alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//    alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
